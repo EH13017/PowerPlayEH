ee32aea9a157c27bf96248ae8b505be074b8dc2e
Adam <ftcteam13017@gmail.com>
2023-02-11 12:05:54 -0600

    Programed Vision

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Adam/VisionTest.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Adam;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Drive To Target", group = "Concept")
+//@Disabled
+public class VisionTest extends LinearOpMode {
+    @Override public void runOpMode() {
+    }
+}
